5614b5091859a71d374bc3b1fa357fc15884221e
JonnierGrajalesAlzate <alzatejonny39@gmail.com>
2026-04-07 00:02:43 -0500

    pagina-corregida

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,14 +1,12 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 const ThemeContext = createContext<any>(null);
 
 export const ThemeProvider = ({ children }: any) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const toggleTheme = () => {
-    setIsDarkMode(prev => !prev);
-  };
- 
+  const toggleTheme = () => setIsDarkMode(prev => !prev);
+
   useEffect(() => {
     document.documentElement.classList.toggle('dark', isDarkMode);
   }, [isDarkMode]);
